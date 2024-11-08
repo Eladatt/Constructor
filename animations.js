5318ff8583d1314767ca6ad92d491943fda0616f
Eladatt <eladatt@gmail.com>
2024-11-08 11:43:50 +0200

    Create animations.js

---
+++ animations.js
@@ -0,0 +1,49 @@
+const animations = [
+    // Blinking Snoopy
+    `   _____ 
+  /     \\
+ /  o o  \\
+(  =^.^=  )
+ \\  ---  /
+  \\_____/`,
+
+    // Waving Charlie Brown
+    `  ___
+ /   \\
+| o o |
+(  ^  )
+ \\___/
+  | |
+ _| |_
+|     |
+ \\___/`,
+
+    // Flying Snoopy on Doghouse
+    `   ___________
+  /           \\
+ /  ___   ___  \\
+|  (o o) (- -)  |
+ \\    <    >   /
+  \\___________/
+      |   |
+      |___|`,
+
+    // Typing Woodstock
+    `  ,~
+ ('v')
+((___))
+ ^   ^`,
+
+    // Growing Snoopy's Thought Bubble
+    `   _____ 
+  /     \\
+ /  o o  \\
+(  =^.^=  )
+ \\  ---  /
+  \\_____/
+    ( )
+   (   )
+  (     )`,
+
+    // Add more animations here...
+];
